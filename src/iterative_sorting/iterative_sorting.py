9e5bdc0accfc5dc72526b7ceecc658bfd3792bfa
Ro McKay Davies <50719703+120Davies@users.noreply.github.com>
2020-04-14 15:39:57 -0600

    day 2 mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,43 @@
-# TO-DO: Complete the selection_sort() function below 
+# TO-DO: Complete the selection_sort() function below
+arr = [7,3,1,8,9,11]
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        smallest = cur_index
+        for x in range(i+1, len(arr)):
+            if arr[x] < arr[smallest]:
+                smallest = x
 
-
-
-        # TO-DO: swap
-
-
-
+        # swap
+        temp = arr[cur_index]
+        arr[cur_index] = arr[smallest]
+        arr[smallest] = temp
 
     return arr
-
+selection_sort(arr)
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    if arr == []:
+        return []
+    if min(arr) < 0:
+        return 'Cannot deal with negative numbers in count_sort.'
+    cntArr = [0] * (max(arr)+1)
+
+    for e in arr:
+        cntArr[e] += 1
+
+    index = 0
+    for i, e in enumerate(cntArr):
+        arr[index:index+e] = [i]*e
+        index += e
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
+# Maybe later.
 def count_sort( arr, maximum=-1 ):
 
     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,17 +2,46 @@
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
-    
+
+    merged_index,A_index,B_index = 0,0,0
+    while A_index < len(arrA) and B_index < len(arrB):
+        if arrA[A_index] < arrB[B_index]:
+            merged_arr[merged_index] = arrA[A_index]
+            A_index += 1
+            merged_index += 1
+        else:
+            merged_arr[merged_index] = arrB[B_index]
+            B_index += 1
+            merged_index += 1
+
+    if A_index == len(arrA):
+        merged_arr[merged_index:] = arrB[B_index:]
+    else:
+        merged_arr[merged_index:] = arrA[A_index:]
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    if arr == []:
+        return []
+    if not isinstance(arr[0], list):
+        arr = list(map(lambda x: [x], arr))
 
-    return arr
+    if len(arr) == 1:
+        return arr[0]
+    if len(arr) % 2 == 0:
+        split1 = arr[:len(arr)//2]
+        split2 = arr[len(arr)//2:]
+        rem = []
+    else:
+        split1 = arr[:len(arr)//2]
+        split2 = arr[len(arr)//2:-1]
+        rem = [arr[-1]]
+    merged_arr = [ merge(a, b) for a, b in zip(split1, split2) ] + rem
 
+    return merge_sort(merged_arr)
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
@@ -20,11 +49,12 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
 
+# check
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
